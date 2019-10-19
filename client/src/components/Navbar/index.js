843392cb1f0d017d3f8f25a5d62ad9c4cc21c57f
Dennis Stötzel <dennis.stoetzel@mgm-sp.com>
2019-10-19 15:40:00 +0700

    auth0 SPA walkthrough

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAuth0 } from "../../auth/react-auth0-spa";
+
+const NavBar = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  return (
+    <div>
+      {!isAuthenticated && (
+        <button onClick={() => loginWithRedirect({})}>Log in</button>
+      )}
+
+      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+
+      {isAuthenticated && (
+        <span>
+          <Link to="/">Home</Link>&nbsp;
+          <Link to="/profile">Profile</Link>
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
