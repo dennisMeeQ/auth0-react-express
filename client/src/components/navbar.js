b77cd607efe8d99204f17d14caa2417e23a56456
Dennis Stötzel <github@meeq.de>
2019-12-21 21:43:10 +0700

    styling navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,31 +2,57 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '../auth/react-auth0-spa';
 
+import './navbar.css';
+
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
   return (
-    <div>
-      <span>
-        <Link to="/">Home</Link>
-        &nbsp;
-        {isAuthenticated && <Link to="/people">People</Link>}
-        &nbsp;
-        {isAuthenticated && <Link to="/profile">Profile</Link>}
-      </span>
-
-      {!isAuthenticated && (
-        <button onClick={() => loginWithRedirect({})} type="button">
-          Log in
-        </button>
-      )}
-
-      {isAuthenticated && (
-        <button onClick={() => logout()} type="button">
-          Log out
-        </button>
-      )}
-    </div>
+    <nav className="menu">
+      <ul className="menu__list">
+        <li className="menu__group">
+          <Link className="menu__link" to="/">
+            Home
+          </Link>
+        </li>
+        {isAuthenticated && (
+          <li className="menu__group">
+            <Link className="menu__link" to="/people">
+              People
+            </Link>
+          </li>
+        )}
+        {isAuthenticated && (
+          <li className="menu__group">
+            <Link className="menu__link" to="/profile">
+              Profile
+            </Link>
+          </li>
+        )}
+        {!isAuthenticated && (
+          <li className="menu__group">
+            <button
+              className="menu__button"
+              onClick={() => loginWithRedirect({})}
+              type="button"
+            >
+              Log in
+            </button>
+          </li>
+        )}
+        {isAuthenticated && (
+          <li className="menu__group">
+            <button
+              className="menu__button"
+              onClick={() => logout()}
+              type="button"
+            >
+              Log out
+            </button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
